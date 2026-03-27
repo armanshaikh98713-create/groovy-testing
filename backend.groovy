166ecf01a77321be9f22470b90f2133eca15c27c
shkarman12 <shaikharman79000@gmail.com>
2026-03-27 22:29:30 +0530

    CODE CHANGE

--- backend.groovy
+++ backend.groovy
@@ -1,9 +1,12 @@
 pipeline {
     agent any
 
-    tools{
+    tools {
         maven 'maven3.9'
-        jdk 'Java21'
+    }
+
+    environment {
+        JAVA_HOME = '/usr/lib/jvm/java-1.21.0-openjdk-amd64'
     }
 
     stages {
@@ -12,11 +15,17 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/armanshaikh98713-create/Java-backend.git'
             }
         }
+        stage('Debug Java') {
+            steps {
+                sh 'echo $JAVA_HOME'
+                sh 'java -version'
+                sh 'mvn -version'
+            }
+        }
         stage('Building Code') {
-            steps{
+            steps {
                 sh 'mvn clean package'
             }
-            
         }
     }
 }
